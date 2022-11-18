e69486c2718e71dc80d9c2fffbaee8dd50919be9
Arthur <arthuremeunome@gmail.com>
2022-11-18 12:26:59 -0300

    feat: news features front-end

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleClick() {
+    document.querySelector('#titulo').innerHTML = 'Saitama';
+}
+
+function handleMove() {
+    document.querySelector('#titulo').innerHTML = 'Saitama';
+}
+
+function handleOut() {
+    document.querySelector('#titulo').innerHTML = "Nation's Gaming sua loja favorita de games";
+}
